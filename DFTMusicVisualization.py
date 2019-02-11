1ad8edcb72c644c528ede6f5b094fd43bf7cb41d
Felix Wei <felix.weichselgartner@t-online.de>
2019-02-11 18:30:11 +0100

    created file for python proof of concept

---
+++ DFTMusicVisualization.py
@@ -0,0 +1,10 @@
+#pip install numpy
+import numpy as np
+from DiscreteFourierTransformation import dft, windowFunctions, zeroPadding, plot
+from NumpyToWAV import ConvertNumpyToWav
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

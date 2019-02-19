c5f480275c3459deacea45e0b240fa4f2ad8c4eb
Marc <zeitlermarc2@gmail.com>
2019-02-19 18:52:31 +0100

    ...

--- DFTMusicVisualization.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include <complex.h>
-#include "DiscreteFourierTransformation/dft.h"
-#include <stdlib.h>
-
-void main() {
-    return;
-}
--- DFTMusicVisualization.ino
+++ DFTMusicVisualization.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
+#define NUM_LEDS 64
+#define DATA_PIN 6
+#define Helligkeit1 10
+#define Helligkeit2 20
+#define laenge 8
+#define hoehe 8
 
+CRGB leds[NUM_LEDS];
+
+int array[8]={8, 8, 8, 8, 8, 8, 8, 8};
+
+void setup() { 
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.clear(true);
+  //FastLED.setBrightness(Helligkeit2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void ausgabe(int *array){
+  for(int i=0; i<8; i++){
+    if(array[i]!=0){
+      for(int j=0; j<array[i]; j++){
+        leds[j+i*8]=CRGB::Blue;
+        leds[0]=CRGB::Blue;
+     }
+     for(int j=array[i]; j<8; j++){
+       leds[j+i*8]=CRGB::Black;
+       leds[0]=CRGB::Blue;
+      }
+   }
+   if(array[i]==0){
+    for(int j=0; j<8; j++){
+     leds[i*8]=CRGB::Black;
+     leds[0]=CRGB::Black;
+    }
+   }
+  }
+}
 
+void loop() { 
+    ausgabe(array);
+    FastLED.show();
+    
 }

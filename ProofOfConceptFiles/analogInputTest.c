b148cf34e0a3ae74aaa8db6c10f4eff6da393c15
FelixWeichselgartner <felix.weichselgartner@t-online.de>
2019-02-25 20:58:22 +0100

    working reading adc and saving sampled values to csv and wav file - yet no music hearable in the wav files

--- ProofOfConceptFiles/analogInputTest.c
+++ ProofOfConceptFiles/analogInputTest.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "tinywav/tinywav.h"
@@ -12,21 +13,35 @@
 #define SecondsOfSampling 10
 #define BLOCK_SIZE 512
 
+//compile command for main function:
+//gcc analogInputTest.c ../mcp3008/mcp3008.c tinywav/tinywav.c -lwiringPi -o analogInputTest
+
 void analogInputTest(int spiChannel, int channelConfig, int channel) {
     time_t start = time(0);
     int delaytime = 1/SAMPLE_RATE;
     TinyWav tw;
     float buffer[BLOCK_SIZE];
-    tinywav_open_write(&tw, NUM_CHANNELS, SAMPLE_RATE, TW_FLOAT32, TW_INLINE, "out/sample.wav");
+    //assertion error if you take a path with a not existing folder:
+    //e.g.: out/sample.wav
+    tinywav_open_write(&tw, NUM_CHANNELS, SAMPLE_RATE, TW_FLOAT32, TW_INLINE, "sample.wav");
+
+    FILE *fp;
+    fp = fopen("sample.csv", "w");
+    if (fp == NULL) {
+	printf("couldnt create file");
+	return;
+    }
 
     do {
         for (int k = 0; k < BLOCK_SIZE; k++) {
             buffer[k] = (float)mcpAnalogRead(spiChannel, channelConfig, channel)*5.0/1023;
 	    delayMicroseconds(delaytime);
+	    fprintf(fp, "%f\n", buffer[k]);
         }
         tinywav_write_f(&tw, buffer, BLOCK_SIZE);
     } while(difftime(time(0), start) < SecondsOfSampling);
 
+    fclose(fp);
     tinywav_close_write(&tw);
     return;
 }

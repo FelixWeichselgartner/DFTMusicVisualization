8e91d96c2ada274ad39aeb144d7ca133bc80b180
Felix Wei <felix.weichselgartner@t-online.de>
2019-02-25 21:29:03 +0100

    convert csv to wav in python - this proofed that the tinywav c lib is working correctly - yet didn't find the reason for no music in wav file

--- ProofOfConceptFiles/NumpyToWAV.py
+++ ProofOfConceptFiles/NumpyToWAV.py
@@ -4,3 +4,26 @@ from scipy.io.wavfile import write
 def ConvertNumpyToWav(name, data):
     scaled = np.int16(data/np.max(np.abs(data)) * 32767)
     write(name + '.wav', 44100, scaled)
+
+def ConvertListToWav(name, theList):
+    arr = np.zeros(len(theList))
+    count = 0
+    for i in theList:
+        arr[count] = i
+        count += 1
+    ConvertNumpyToWav(name, arr)
+
+
+def main():
+    signal = []
+    print(signal)
+    with open('sample.csv') as f:
+        # with: i = index and v = value
+        for i, v in enumerate(f):
+            signal.append(v)
+            if (i < 16):
+                print(f'{i}: {signal[i]} == {v}')
+        ConvertListToWav('sample_python', signal)
+
+if __name__ == "__main__":
+    main()

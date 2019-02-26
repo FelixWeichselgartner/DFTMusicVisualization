b1bd38c84599f67bf5b22b672a04373d841d581a
FelixWeichselgartner <felix.weichselgartner@t-online.de>
2019-02-26 21:26:09 +0100

    CMake version running

--- DFTMusicVisualization.c
+++ DFTMusicVisualization.c
@@ -6,19 +6,17 @@
 #include <complex.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
-#include "DiscreteFourierTransformation/dft.h"
-#include "mcp3008/mcp3008.h"
+#include "mylib/DiscreteFourierTransformation/c/dft.h"
+#include "mylib/mcp3008/mcp3008.h"
 
 #define true 1
 #define false 0
 
+#define debug true
+
 #define ARRAYWIDTH 12
 #define samplingFrequency 44100
 #define deltaT 1/samplingFrequency*1000*1000
-
-//compile command:
-//gcc DFTMusicVisualization.c DiscreteFourierTransformation/dft.c mcp3008/mcp3008.c -o DFTMusicVisualization -lm -lwiringPi
-//gcc DFTMusicVisualization.c DiscreteFourierTransformation/dft.c mcp3008/mcp3008.c ProofOfConceptFiles/tinywav/tinywav.c ProofOfConceptFiles/analogInputTest.c -o DFTMusicVisualization -lm -lwiringPi -lrt
 
 static int spi;
 
@@ -29,7 +27,6 @@ short *signal;
 float complex *fourier;
 int display[ARRAYWIDTH];
 int norm = pow(2, 10-4); //pow(2, 4) = 8
-int norm8bit;
 
 int integrate(int start, int end) {
     float retval = 0;
@@ -57,14 +54,14 @@ void sample() {
         //later:
         //transform each channel on its own and display left channel on left side of matrix
         //right channel on right side of the matrix
-        signal[i] = (mcpAnalogRead(spiChannel, channelConfig, channel1) + mcpAnalogRead(spiChannel, channelConfig, channel2))/(2*norm);
+        signal[i] = (mcpAnalogRead(spiChannel, channelConfig, channel1) + mcpAnalogRead(spiChannel, channelConfig, channel2))/(2);
         delayMicroseconds(deltaT);
     }
 }
 
 void FormToMatrix() {
     int max = length/2;
-    int step = norm8bit = max/ARRAYWIDTH;
+    int step = max/ARRAYWIDTH;
     int start = 0, end = step;
     for (int i = 0; i < ARRAYWIDTH; i++) {
         display[i] = integrate(start, end);
@@ -75,18 +72,18 @@ void FormToMatrix() {
 
 void normTo8Bit() {
     for (int i = 0; i < ARRAYWIDTH; i++) {
-        display[i] = display[i]/norm8bit;
+        display[i] = display[i]*8/pow(2, 10);
     }
 }
 
 void ConsoleOutput() {
     printf("Matrix:\n");
     for(int i = 0; i < ARRAYWIDTH; i++) {
-        printf("%i ", i);
+        printf("%2i ", i);
     }
     printf("\n");
     for(int i = 0; i < ARRAYWIDTH; i++) {
-        printf("%i ", display[i]);
+        printf("%2i ", display[i]);
     }
     printf("\n");
 }
@@ -100,13 +97,11 @@ void loop() {
     fourier = fft(signal, length);
     FormToMatrix();
     normTo8Bit();
-    ConsoleOutput();
+    if (debug) {
+	ConsoleOutput();
+    }
     MatrixOutput();
     free(fourier);
-}
-
-void TestSoundSampling(int spiChannel, int channelConfig, int channel) {
-    analogInputTest(spiChannel, channelConfig, channel);
 }
 
 void main() {
@@ -116,6 +111,9 @@ void main() {
     //while button wasnt pushed
     while(running) {
         loop();
+	if (debug) {
+	    delay(1);
+	}
     }
 
     free(signal); free(fourier);
--- README.md
+++ README.md
@@ -7,3 +7,13 @@ run in the lib folder:
 ```
 git clone https://github.com/jgarff/rpi_ws281x.git
 ```
+then you will have to delete the main.c file in the new folder lib/rpi_ws281x
+
+
+then run:
+```
+cmake .
+```
+```
+make
+```

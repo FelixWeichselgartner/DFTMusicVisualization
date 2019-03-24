016a8343a08a4fcff98af4a4685ed2bfa7f9e91a
Felix Wei <felix.weichselgartner@t-online.de>
2019-03-24 11:49:02 +0100

    new python skript

--- ProofOfConceptFiles/SampeTest.py
+++ ProofOfConceptFiles/SampeTest.py
@@ -1,11 +1,43 @@
-import sounddevice as sd
-import numpy as np
+# -*- coding: UTF-8 -*-
+""" Record voice
+"""
 
-duration = 10
-fs = 44100
+from lib import BUTTON, DEVICE, AUDIBLE_PATH
 
-myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
-sd.wait()
+import alsaaudio
+import time
 
-for i in range(len(myrecording)):
-    print(myrecording[i])
+
+class RecordVoice(object):
+    """ Record voice Module
+    """
+
+    RECORD_FILE = "recording.wav"
+    RESPONSE_FILE = "response.mp3"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def record():
+        """ Record voice
+        """
+        seconds = 10
+        t_end = time.time() + 60 * seconds
+
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,
+                            alsaaudio.PCM_NORMAL, DEVICE)
+        inp.setchannels(1)
+        inp.setrate(44100)
+        inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp.setperiodsize(500)
+
+        audio = ""
+        # we keep recording while the button is pressed
+        while time.time() < t_end:
+            valid, data = inp.read()
+            if valid:
+                audio += data
+        save_audio = open("sample.wav", 'w')
+        save_audio.write(audio)
+        save_audio.close()
